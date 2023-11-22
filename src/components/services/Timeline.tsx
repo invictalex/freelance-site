ede8af77cc02e99f74906ced32ae46ddbb4fd2cd
invictalex <alexchristodoulou@ymail.com>
2023-11-22 11:50:00 +0000

    update

--- src/components/services/Timeline.tsx
+++ src/components/services/Timeline.tsx
@@ -10,19 +10,20 @@ export default function Timeline(props) {
      "You sign off visual draft", "Product delivered", "2 rounds of feedback & amends", "The product is yours!"]
 
     const listJSX = list.map((item, i) => (
-        <div className="flex items-center z-10 relative">
-
-            <motion.span className="w-5 h-5 rounded-full  mr-10 inline"
-            initial={{scale: 0.5, backgroundColor: "rgb(49 52 59)", border: "solid 4px rgb(49 52 59)"}}
-            whileInView={{scale: 1, backgroundColor: "rgb(242 242 242)", border: "solid 4px rgb(49 52 59)" }}
-            viewport={{ root: ref, margin: "-200px 0px -200px 0px"}}
+        <div className="flex items-center z-10 relative"
+        
+        >
+            <motion.span className="w-5 h-5 rounded-full outline outline-8 outline-slate mr-10 inline"
+            initial={{scale: 0.5, backgroundColor: "rgb(148 161 184)"}}
+            whileInView={{scale: 1, backgroundColor: "#fff"}}
+            viewport={{ root: ref, margin: "-120px 0px -120px 0px"}}
             ></motion.span>
 
             <motion.li key={i}
-                className="list-none inline my-8 z-100 border-2 border-slate rounded-lg"
-                initial={{fontSize: "1rem", opacity: 0.5, padding: "1rem 2rem 1rem 2rem"}}
-                whileInView={{fontSize: "1.5rem", opacity: 1,  padding: "2rem 3rem 2rem 3rem"}}
-                viewport={{root: ref, margin: "-200px 0px -200px 0px"}}
+                className={`list-none inline my-8 z-100 `}
+                initial={{fontSize: "1rem", opacity: 0.5}}
+                whileInView={{fontSize: "1.5rem", opacity: 1}}
+                viewport={{root: ref, margin: "-120px 0px -120px 0px"}}
                 
             >
                 {item}
@@ -33,15 +34,12 @@ export default function Timeline(props) {
 
 
     return(
-        <div className="w-full  relative h-[600px] flex justify-center">
-            <div ref={ref} className="  pl-20  w-[600px] h-[500px] overflow-scroll scrollbar-hide">
-                <span className="absolute h-[2000px] w-[2px] bg-slate ml-[0.59rem]"></span>
-                <div className="mb-20">
-                
-                    {listJSX}
+        <div ref={ref} className="w-full text-white pl-20  h-[300px] overflow-scroll relative scrollbar-hide">
+            <span className="absolute h-[620px] w-[2px] bg-blue-lt ml-[0.59rem]"></span>
+           <div className="mb-20">
+           {listJSX}
 
-                </div>
-            </div>
+           </div>
         </div>
     )
    
